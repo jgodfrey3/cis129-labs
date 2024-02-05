0c06b10419a3172003f14fc9b9e0c5f804269981
jgodfrey3 <158596982+jgodfrey3@users.noreply.github.com>
2024-02-05 11:16:11 -0700

    replaces hello world with short story

--- cis129_lab02_shortStory.py
+++ cis129_lab02_shortStory.py
@@ -1,1 +1,9 @@
-print('Hello, World!')
+#Author: Jared Godfrey
+#My first Python Lab in my CIS course
+#Prints a short poem about a spider
+print('Once upon a time there was a small spider down by the lake.')
+print('The little spider was climbing across very big rake.')
+print('A farmer came by, fetching water for the night')
+print('But he stepped on the rake and sent it upright'!)
+print('The farmer soon after filled his bucket to the brim;')
+print('But the poor little spider was sent for a swim.')
